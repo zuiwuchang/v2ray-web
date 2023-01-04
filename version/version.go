15198265e415cec7e84ed89fdbdc2e8c3dfb8603
zuiwuchang <zuiwuchang@gmail.com>
2023-01-04 20:26:28 +0800

    view

--- version/version.go
+++ version/version.go
@@ -1,7 +1,7 @@
 package version
 
 var (
-	Version = `v1.5.1`
-	Commit = `09028f255192a3b0a562e4521839831c4b7f056a`
-	Date = `2022-09-08 11:56:39`
+	Version = `v1.5.2`
+	Commit = `127d200cb6c814168bed5034c159406a1edc629e`
+	Date = `2022-09-08 17:21:51`
 )
