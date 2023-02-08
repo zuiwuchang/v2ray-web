fff9c4389c075b89394d782db9a3c0a2e78ef28a
zuiwuchang <zuiwuchang@gmail.com>
2023-02-08 22:47:06 +0800

    docker init

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,6 +1,40 @@
 #!/bin/bash
-
+set -e
 if [[ "$@" == "default-command" ]];then
+    # import init settings
+    if [[ ! -f  /data/v2ray-web.db ]]; then
+        if [[ "$V2RAY_SETTINGS" != "" ]]; then
+            /opt/v2ray-web/v2ray-web import --db /data/v2ray-web.db -s "$V2RAY_SETTINGS"
+        fi
+        if [[ "$V2RAY_STRATEGY" != "" ]]; then
+            /opt/v2ray-web/v2ray-web import --db /data/v2ray-web.db --strategy "$V2RAY_STRATEGY"
+        fi
+        if [[ "$V2RAY_V2RAY" != "" ]]; then
+            /opt/v2ray-web/v2ray-web import --db /data/v2ray-web.db -v "$V2RAY_V2RAY"
+        fi
+        if [[ "$V2RAY_SUBSCRIPTION" != "" ]]; then
+            /opt/v2ray-web/v2ray-web import --db /data/v2ray-web.db --subscription "$V2RAY_SUBSCRIPTION"
+        fi
+        if [[ "$V2RAY_IPTABLES" != "" ]]; then
+            /opt/v2ray-web/v2ray-web import --db /data/v2ray-web.db --iptables "$V2RAY_IPTABLES"
+        fi
+        if [[ "$V2RAY_IPTABLES_VIEW" != "" ]]; then
+            /opt/v2ray-web/v2ray-web import --db /data/v2ray-web.db --iptables-view "$V2RAY_IPTABLES_VIEW"
+        fi
+        if [[ "$V2RAY_IPTABLES_CLEAR" != "" ]]; then
+            /opt/v2ray-web/v2ray-web import --db /data/v2ray-web.db --iptables-clear "$V2RAY_IPTABLES_CLEAR"
+        fi
+        if [[ "$V2RAY_IPTABLES_INIT" != "" ]]; then
+            /opt/v2ray-web/v2ray-web import --db /data/v2ray-web.db --iptables-init "$V2RAY_IPTABLES_INIT"
+        fi
+        if [[ "$V2RAY_USER" != "" ]]; then
+            /opt/v2ray-web/v2ray-web import --db /data/v2ray-web.db -u "$V2RAY_USER"
+        fi
+        if [[ "$V2RAY_PROXY" != "" ]]; then
+            /opt/v2ray-web/v2ray-web import --db /data/v2ray-web.db -p "$V2RAY_PROXY"
+        fi
+    fi
+
     exec /opt/v2ray-web/v2ray-web web --no-upgrade
 else
     exec "$@"
